9f0ac4c91ccdcf10290fc4a7403ca3221cb7b37d
YunChangLi <bruce6531@gmail.com>
2021-11-07 00:16:08 +0800

    add new scene

--- Assets/Scripts/UI/DebugUIController.cs
+++ Assets/Scripts/UI/DebugUIController.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class DebugUIController : MonoBehaviour
 {
-    public string FilePath;
-    public string FilePath2;
     public int ModeNumber;
 
     public string[] StatusMode = { "Concatenate", "SkeletonPathEditing", "ModelPathEditing" };
@@ -43,40 +41,14 @@ public class DebugUIController : MonoBehaviour
             ModeNumber = 2;
             SceneManager.LoadScene("TestScene");
         }
+        if (GUILayout.Button("Load BVHs", GUILayout.Width(300), GUILayout.Height(80)))
+        {
+            ModeNumber = 3;
+            SceneManager.LoadScene("BVHsScene");
+        }
 
         GUILayout.EndHorizontal();
 
-        /*if (ModeNumber == 0)
-        {
-            GUILayout.BeginVertical();
-
-            //GUILayout.BeginArea(new Rect(Screen.width * 0.05f, Screen.height * 0.5f, Screen.width * 0.9f, Screen.height * 0.01f));
-            if (GUILayout.Button("Load BVH File 1", GUILayout.Width(300), GUILayout.Height(80)))
-            {
-                FilePath = "";
-                FilePath = EditorUtility.OpenFilePanel("Load BVH file 1", "", "");
-            }
-            GUILayout.Space(10);
-            if (GUILayout.Button("Load BVH File 2", GUILayout.Width(300), GUILayout.Height(80)))
-            {
-                FilePath2 = "";
-                FilePath2 = EditorUtility.OpenFilePanel("Load BVH file 2", "", "");
-            }
-            GUILayout.EndVertical();
-        }
-        else if (ModeNumber == 1)
-        {
-            //GUILayout.BeginArea(new Rect(Screen.width * 0.05f, Screen.height * 0.5f, Screen.width * 0.9f, Screen.height * 0.01f));
-            if (GUILayout.Button("Load BVH File ", GUILayout.Width(300), GUILayout.Height(80)))
-            {
-                FilePath = "";
-                FilePath2 = "";
-                FilePath = EditorUtility.OpenFilePanel("Load BVH file ", "", "");
-            }
-        }
-        else 
-        {
-            FilePath = Application.dataPath + "/Resources/08_01.bvh";
-        }*/
+       
     }
 }
